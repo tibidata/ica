1bed4574760f168d25b4d8583d63f1caec032654
tibortamas <tibor.tamas96@gmail.com>
2023-04-24 10:07:56 +0200

    Added code snippet for testing

---
+++ test.py
@@ -0,0 +1,27 @@
+import numpy as np
+from src import model
+from scipy import signal
+
+# Set a seed for the random number generator for reproducibility
+np.random.seed(23)
+
+# Number of samples
+ns = np.linspace(0, 50, 500)
+
+# Source matrix
+S = np.array([np.sin(ns * 1),
+              signal.sawtooth(ns * 1.9),
+              np.random.random(len(ns))]).T
+
+# Mixing matrix
+A = np.array([[0.5, 1, 0.2],
+              [1, 0.5, 0.4],
+              [0.5, 0.8, 1]])
+
+# Mixed signal matrix
+X = S.dot(A).T
+
+model = model.IndependentComponentAnalysis(X=X)
+
+model.predict()
+model.plot()
